97524fc316c348dff54c04943c5ca5c09f11b2ba
Snir Lugassy <snirlugassy@yahoo.com>
2022-01-03 13:05:31 +0200

    removed list[str]

--- preprocessing.py
+++ preprocessing.py
@@ -8,7 +8,7 @@ STOPWORDS = stopwords.words('english')
 HTML_TAG = re.compile('<.*?>')
 
 class DataProcessing:
-    def __init__(self, input_files: list[str], output_file: str, nrows=20000):
+    def __init__(self, input_files: list, output_file: str, nrows=20000):
         self.files = input_files
     
     def run(self):
