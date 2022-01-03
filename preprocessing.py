7b1e0a24d052d8bc8ed69d36a230fc2daebe5c16
Snir Lugassy <snirlugassy@yahoo.com>
2022-01-03 09:55:04 +0200

    sdf

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,11 @@
+import re
+import sys
+import pandas as pd
+import numpy as np
+
+
 class DataProcessing:
-    def __init__(self, input_files:list[str], output_file:str, nrows=20000):
+    def __init__(self, input_files: list[str], output_file: str, nrows=20000):
         self.files = input_files
     
     def run(self):
@@ -10,3 +16,8 @@ class TextNormalization:
     @staticmethod
     def normalize(text):
         return text
+
+
+if __name__ == '__main__':
+    data_path = sys.argv[1]
+    data = pd.read_csv(data_path, nrows=10000)
