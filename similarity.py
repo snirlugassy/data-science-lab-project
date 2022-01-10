920c87578d98f389d0a94a08f981b0d7e96dacc5
Snir Lugassy <snirlugassy@yahoo.com>
2022-01-10 12:37:20 +0200

    added clustering

--- similarity.py
+++ similarity.py
@@ -1,8 +1,24 @@
-def similarity(x,y):
-    pass
+import pickle
+from sklearn.cluster import KMeans, MiniBatchKMeans
 
-def sparse_similarity(x,y):
-    pass
 
-def rank(x,corpus):
-    pass
+def kmeans_clustering(vectors):
+    with open('vectorizer.pkl', 'rb') as f:
+        X = pickle.load(f)
+
+    kmeans = KMeans(n_clusters=10, random_state=0, max_iter=10, verbose=True)
+    labels = kmeans.fit_predict(vectors)
+
+    with open('kmeans_model.pkl', 'wb') as f:
+        pickle.dump(kmeans, f)
+
+    with open('doc2cluster.pkl', 'wb') as f:
+        pickle.dump(labels, f)
+    
+    return kmeans, labels
+
+if __name__ == '__main__':
+    with open('vectors.pkl', 'rb') as f:
+        vectors = pickle.load(f)
+    
+    kmeans, labels = kmeans_clustering(vectors)
